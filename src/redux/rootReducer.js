d5f3f01c123b7eae6896bddb19d0849cae237ef8
mayank vyas <mayank.vyas@square-bits.com>
2022-12-13 14:03:07 +0530

    Authentication of vuexy removed

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,6 @@
 // ** Reducers Imports
 import navbar from './navbar'
 import layout from './layout'
-import auth from './authentication'
 import authSlice from './authSlice'
 
 const rootReducer = {
