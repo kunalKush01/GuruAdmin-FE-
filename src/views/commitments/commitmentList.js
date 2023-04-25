5dac13c4790cf6aa78e4fa6afbab044ec80bc90a
govind_sharma <mayank.vyas@square-bits.com>
2023-04-25 10:58:00 +0530

    commitment align

--- src/views/commitments/commitmentList.js
+++ src/views/commitments/commitmentList.js
@@ -251,7 +251,7 @@ export default function Commitment() {
 
       <div>
         <div className="d-lg-flex justify-content-between align-items-center">
-          <div className="d-flex align-items-center mb-2">
+          <div className="d-flex align-items-center mb-2 mb-lg-0">
             <img
               src={arrowLeft}
               className="me-2  cursor-pointer align-self-center"
