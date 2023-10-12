67315d3d5a90dfec29737c18669f80cf2367f750
govind <govind.sharma@squarebits.com>
2023-10-12 15:14:52 +0530

    domain cookie

--- src/utility/formater/index.js
+++ src/utility/formater/index.js
@@ -9,14 +9,14 @@ export function ConverFirstLatterToCapital (str){
 export function getCookie(cookieName) {
     const name = cookieName + "=";
     const decodedCookie = decodeURIComponent(document.cookie);
-    const cookieArray = decodedCookie.split(';');
- 
+    const cookieArray = decodedCookie.split(";");
+  
     for (let i = 0; i < cookieArray.length; i++) {
-        let cookie = cookieArray[i].trim();
-        if (cookie.indexOf(name) === 0) {
-            return cookie.substring(name.length, cookie.length);
-        }
+      let cookie = cookieArray[i].trim();
+  
+      if (cookie.indexOf(name) === 0) {
+        return cookie.substring(name.length, cookie.length);
+      }
     }
- 
-    return "";
- }
+    return null; // Return null if the cookie with the specified name is not found
+  }
