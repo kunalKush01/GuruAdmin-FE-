98912a668f242949bf9593f516a0849458f7ccea
govind <govind.sharma@squarebits.com>
2023-10-12 12:50:46 +0530

    cookie

--- src/utility/formater/index.js
+++ src/utility/formater/index.js
@@ -5,3 +5,18 @@
 export function ConverFirstLatterToCapital (str){
     return (str.charAt(0).toUpperCase() + str.slice(1))
 }
+
+export function getCookie(cookieName) {
+    const name = cookieName + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
+ 
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i].trim();
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+ 
+    return "";
+ }
