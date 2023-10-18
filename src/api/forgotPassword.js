e19af0ff295a09dedb22eb94c4b7e3e09d466017
govind <govind.sharma@squarebits.com>
2023-10-18 11:07:16 +0530

    url

--- src/api/forgotPassword.js
+++ src/api/forgotPassword.js
@@ -4,13 +4,13 @@ import { callApi } from "../utility/utils/callApi";
 export const forgotPassword = (payload) =>
   callApi({
     requestFunction: (axios) =>
-      axios.post(`${API_BASE_URL}auth/forgot-password`, payload),
+      axios.post(`${API_AUTH_URL}auth/forgot-password`, payload),
   });
 
 export const resetPassword = (payload) =>
   callApi({
     requestFunction: (axios) =>
-      axios.post(`${API_BASE_URL}auth/reset-password`, payload),
+      axios.post(`${API_AUTH_URL}auth/reset-password`, payload),
       showToastOnError:true
       
   });
