021eb8a97d6512c51c96ca123815cf500eab6569
govind <govind.sharma@squarebits.com>
2023-11-01 18:11:56 +0530

    background image

--- src/views/ResetPassword/index.js
+++ src/views/ResetPassword/index.js
@@ -179,8 +179,9 @@ const ResetPassWord = () => {
             <img
               className="img-fluid w-100 h-100"
               src={
-                loginPageData?.image !== "" || loginPageData?.image
-                  ? loginPageData?.image
+                (loginPageData && loginPageData?.profilePhoto !== "") ||
+                loginPageData?.profilePhoto
+                  ? loginPageData?.profilePhoto
                   : source
               }
               alt="Login Cover"
