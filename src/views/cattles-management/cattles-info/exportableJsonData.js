214b893e7816a1e98bbb544eaf2cc4534b678be2
Govind Sharma <govind.sharma@sqaurebits.com>
2024-02-28 18:35:04 +0530

    export cattle info

--- src/views/cattles-management/cattles-info/exportableJsonData.js
+++ src/views/cattles-management/cattles-info/exportableJsonData.js
@@ -3,20 +3,38 @@ import moment from "moment";
 export const exportCattleJson = (data) => {
   return data.map((item) => ({
     cattleId: item?.tagId,
-    ownerId: item?.ownerId,
-    type: item?.type,
     motherId: item?.motherId ?? "N/A",
+    type: item?.type,
     breed: item?.breed,
     dateOfBirth: moment(item?.dob).format(" DD MMM YYYY"),
     age: item?.age,
-    isPregnant:
-      // <div style={{ padding: "16px" }}>
-      item?.isPregnant ? "YES" : "NO",
-    // </div>
+    purchaseDate: moment(item?.purchaseDate).format(" DD MMM YYYY"),
+    purchasePrice: item?.purchaseDate ?? "N/A",
+    source: item?.source ?? "N/A",
+    ownerName: item?.ownerName ?? "N/A",
+    ownerNumber: item?.ownerMobile
+      ? "+" + item?.ownerCountryCode + item?.ownerMobile
+      : "N/A",
+    ownerId: item?.ownerId,
+    isDead: item?.isDead ? "YES" : "NO",
+    deathReason: item?.deathReason ?? "-",
+    isSold: item?.isSold ? "YES" : "NO",
+    purchaserName: item?.purchaserName ?? "-",
+    purchaserNumber: item?.purchaserMobile
+      ? "+" + item?.purchaserCountryCode + item?.purchaserMobile
+      : "N/A",
+    soldDate: item?.soldDate
+      ? moment(item?.soldDate).format(" DD MMM YYYY")
+      : "N/A",
+    soldPrice: item?.soldPrice ?? "N/A",
     isMilking: item?.isMilking ? "YES" : "NO",
+    milkQuantity: item?.milkQuantity ?? "N/A",
+    isPregnant: item?.isPregnant ? "YES" : "NO",
+    deliveryDate: item?.deliveryDate
+      ? moment(item?.deliveryDate).format(" DD MMM YYYY")
+      : "N/A",
     pregnancyDate: item?.pregnancyDate
       ? moment(item?.pregnancyDate).format(" DD MMM YYYY")
       : "N/A",
-    milkQuantity: item?.milkQuantity ?? "N/A",
   }));
 };
