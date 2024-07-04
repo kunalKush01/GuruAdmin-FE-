9be2a5017e1de706419c81100af886c88e0666d1
the-onelazyDev <amitcse21@gmail.com>
2024-07-04 15:41:30 +0530

    subscribedUser fixed

--- src/components/subscribedUser/subscribedUserForm.js
+++ src/components/subscribedUser/subscribedUserForm.js
@@ -71,7 +71,7 @@ export default function SubscribedUserForm({
         setLoading(false);
         addDonationUser
           ? history.push(
-              `/${redirectTo}/add?page=${currentPage}&category=${currentCategory}&subCategory=${currentSubCategory}&filter=${currentFilter}`
+              `/${redirectTo}/add?page=${currentPage}&category=${currentCategory}&subCategory=${currentSubCategory}&filter=${currentFilter}&mobileNumber=${getNumber}`
             )
           : history.push("/subscribed-user");
       } else if (data?.error) {
