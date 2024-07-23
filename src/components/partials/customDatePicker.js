34aac326186280d499b141729b1cb1d5510ad799
Vaibhav Jain <vaibhav.jain@betterplace.co.in>
2024-07-23 15:27:42 +0530

    css cleanup

--- src/components/partials/customDatePicker.js
+++ src/components/partials/customDatePicker.js
@@ -7,7 +7,7 @@ import "../../assets/scss/common.scss";
 
 export default function CustomDatePicker({ ...props }) {
   return (
-    <div className="customdatepickerwraper">
+    <div className="customdatepickerwrapper">
       <DatePicker
         yearDropdownItemNumber={50}
         showYearDropdown
