b6116b4cadd7d6cee54e4bbff924661abd491580
Vaibhav Jain <vaibhav.jain@betterplace.co.in>
2024-07-23 11:34:56 +0530

    fixing css order

--- src/components/partials/FormikRangeDatePicker.js
+++ src/components/partials/FormikRangeDatePicker.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import "react-datepicker/dist/react-datepicker-cssmodules.css";
+import "react-datepicker/dist/react-datepicker.css";
 import styled from "styled-components";
 import { Trans } from "react-i18next";
 import { useField } from "formik";
