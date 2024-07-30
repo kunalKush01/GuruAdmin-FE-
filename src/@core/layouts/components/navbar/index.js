046e8961884b925baa4502a1e680373c91ac032c
Vaibhav Jain <vaibhav.jain@betterplace.co.in>
2024-07-30 19:01:37 +0530

    fix for tab font size

--- src/@core/layouts/components/navbar/index.js
+++ src/@core/layouts/components/navbar/index.js
@@ -1,31 +1,34 @@
 // ** React Imports
-import { Fragment } from 'react'
+import { Fragment } from "react";
 
 // ** Custom Components
-import NavbarUser from './NavbarUser'
+import NavbarUser from "./NavbarUser";
 
 // ** Third Party Components
-import { Menu } from 'react-feather'
+import { Menu } from "react-feather";
 
 // ** Reactstrap Imports
-import { NavItem, NavLink } from 'reactstrap'
+import { NavItem, NavLink } from "reactstrap";
 
-const ThemeNavbar = props => {
+const ThemeNavbar = (props) => {
   // ** Props
-  const { skin, setSkin, setMenuVisibility } = props
+  const { skin, setSkin, setMenuVisibility } = props;
 
   return (
     <Fragment>
-      <ul className='navbar-nav'>
-        <NavItem className='mobile-menu me-auto d-flex align-items-center'>
-          <NavLink className='nav-menu-main menu-toggle hidden-xs is-active' onClick={() => setMenuVisibility(true)}>
-            <Menu className='ficon' />
+      <ul className="navbar-nav">
+        <NavItem className="mobile-menu me-auto d-flex align-items-center">
+          <NavLink
+            className="nav-menu-main menu-toggle hidden-xs is-active"
+            onClick={() => setMenuVisibility(true)}
+          >
+            <Menu className="ficon" />
           </NavLink>
         </NavItem>
       </ul>
       <NavbarUser skin={skin} setSkin={setSkin} />
     </Fragment>
-  )
-}
+  );
+};
 
-export default ThemeNavbar
+export default ThemeNavbar;
