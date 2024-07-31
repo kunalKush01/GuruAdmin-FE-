44738e8e2a7d792cb67cb4e1449acf65aff65876
Vaibhav Jain <vaibhav.jain@betterplace.co.in>
2024-07-31 12:14:22 +0530

    fixes

--- src/@core/layouts/components/menu/vertical-menu/VerticalNavMenuGroup.js
+++ src/@core/layouts/components/menu/vertical-menu/VerticalNavMenuGroup.js
@@ -1,19 +1,19 @@
 // ** React Imports
-import { useEffect } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 // ** Third Party Components
-import classnames from 'classnames'
+import classnames from "classnames";
 
 // ** Reactstrap Imports
-import { Collapse, Badge } from 'reactstrap'
+import { Collapse, Badge } from "reactstrap";
 
 // ** Vertical Menu Items Component
-import VerticalNavMenuItems from './VerticalNavMenuItems'
+import VerticalNavMenuItems from "./VerticalNavMenuItems";
 
 // ** Utils
-import { hasActiveChild, removeChildren } from '@layouts/utils'
-import { Trans } from 'react-i18next'
+import { hasActiveChild, removeChildren } from "@layouts/utils";
+import { Trans } from "react-i18next";
 
 const VerticalNavMenuGroup = ({
   item,
@@ -33,23 +33,23 @@ const VerticalNavMenuGroup = ({
   ...rest
 }) => {
   // ** Hooks
-  const location = useLocation()
+  const location = useLocation();
 
   // ** Current Val
-  const currentURL = useLocation().pathname
+  const currentURL = useLocation().pathname;
 
   // ** Toggle Open Group
   const toggleOpenGroup = (item, parent) => {
-    let openGroup = groupOpen
-    const activeGroup = groupActive
+    let openGroup = groupOpen;
+    const activeGroup = groupActive;
 
     // ** If Group is already open and clicked, close the group
     if (openGroup.includes(item.id)) {
-      openGroup.splice(openGroup.indexOf(item.id), 1)
+      openGroup.splice(openGroup.indexOf(item.id), 1);
 
       // ** If clicked Group has open group children, Also remove those children to close those groups
       if (item.children) {
-        removeChildren(item.children, openGroup, groupActive)
+        removeChildren(item.children, openGroup, groupActive);
       }
     } else if (
       activeGroup.includes(item.id) ||
@@ -62,102 +62,109 @@ const VerticalNavMenuGroup = ({
         !activeGroup.includes(item.id) &&
         currentActiveGroup.includes(item.id)
       ) {
-        activeGroup.push(item.id)
+        activeGroup.push(item.id);
       } else {
-        activeGroup.splice(activeGroup.indexOf(item.id), 1)
+        activeGroup.splice(activeGroup.indexOf(item.id), 1);
       }
 
       // ** Update Active Group
-      setGroupActive([...activeGroup])
+      setGroupActive([...activeGroup]);
     } else if (parent) {
       // ** If Group clicked is the child of a open group, first remove all the open groups under that parent
       if (parent.children) {
-        removeChildren(parent.children, openGroup, groupActive)
+        removeChildren(parent.children, openGroup, groupActive);
       }
 
       // ** After removing all the open groups under that parent, add the clicked group to open group array
       if (!openGroup.includes(item.id)) {
-        openGroup.push(item.id)
+        openGroup.push(item.id);
       }
     } else {
       // ** If clicked on another group that is not active or open, create openGroup array from scratch
 
       // ** Empty Open Group array
-      openGroup = []
+      openGroup = [];
 
       // ** Push current clicked group item to Open Group array
       if (!openGroup.includes(item.id)) {
-        openGroup.push(item.id)
+        openGroup.push(item.id);
       }
     }
-    setGroupOpen([...openGroup])
-  }
+    setGroupOpen([...openGroup]);
+  };
 
   // ** On Group Item Click
   const onCollapseClick = (e, item) => {
-    toggleOpenGroup(item, parentItem)
+    toggleOpenGroup(item, parentItem);
 
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
 
   // ** Checks url & updates active item
   useEffect(() => {
     if (hasActiveChild(item, currentURL)) {
-      if (!groupActive.includes(item.id)) groupActive.push(item.id)
+      if (!groupActive.includes(item.id)) groupActive.push(item.id);
     } else {
-      const index = groupActive.indexOf(item.id)
-      if (index > -1) groupActive.splice(index, 1)
+      const index = groupActive.indexOf(item.id);
+      if (index > -1) groupActive.splice(index, 1);
     }
-    setGroupActive([...groupActive])
-    setCurrentActiveGroup([...groupActive])
-    setGroupOpen([])
-  }, [location])
+    setGroupActive([...groupActive]);
+    setCurrentActiveGroup([...groupActive]);
+    setGroupOpen([]);
+  }, [location]);
 
   // ** Returns condition to add open class
-  const openClassCondition = id => {
+  const openClassCondition = (id) => {
     if ((menuCollapsed && menuHover) || menuCollapsed === false) {
       if (groupActive.includes(id) || groupOpen.includes(id)) {
-        return true
+        return true;
       }
     } else if (
       groupActive.includes(id) &&
       menuCollapsed &&
       menuHover === false
     ) {
-      return false
+      return false;
     } else {
-      return null
+      return null;
     }
-  }
+  };
 
   return (
     <li
-      className={classnames('nav-item has-sub', {
+      className={classnames("nav-item has-sub", {
         open: openClassCondition(item.id),
-        'menu-collapsed-open': groupActive.includes(item.id),
-        'sidebar-group-active':
+        "menu-collapsed-open": groupActive.includes(item.id),
+        "sidebar-group-active":
           groupActive.includes(item.id) ||
           groupOpen.includes(item.id) ||
-          currentActiveGroup.includes(item.id)
+          currentActiveGroup.includes(item.id),
       })}
     >
       <Link
-        className='d-flex align-items-center'
-        to='/'
-        onClick={e => onCollapseClick(e, item)}
+        className="d-flex align-items-center"
+        to="/"
+        onClick={(e) => onCollapseClick(e, item)}
       >
-        {item.icon}
-        <span className='menu-title text-truncate'><Trans i18nKey={item?.name}/></span>
+        <img
+          src={item.icon}
+          height={20}
+          width={20}
+          style={{ marginLeft: "0px", marginRight: "10px" }}
+        />
+        <span className="menu-title text-truncate">
+          <Trans i18nKey={item?.name} />
+        </span>
 
         {item.badge && item.badgeText ? (
-          <Badge className='ms-auto me-1' color={item.badge} pill>
+          <Badge className="ms-auto me-1" color={item.badge} pill>
             {item.badgeText}
           </Badge>
         ) : null}
       </Link>
 
       {/* Render Child Recursively Through VerticalNavMenuItems Component */}
-      <ul className='menu-content'>
+      <ul className="menu-content">
         <Collapse
           isOpen={
             (groupActive && groupActive.includes(item.id)) ||
@@ -184,7 +191,7 @@ const VerticalNavMenuGroup = ({
         </Collapse>
       </ul>
     </li>
-  )
-}
+  );
+};
 
-export default VerticalNavMenuGroup
+export default VerticalNavMenuGroup;
