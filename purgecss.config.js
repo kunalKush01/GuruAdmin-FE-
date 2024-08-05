4346d65788bde742f4cef2aebc00e6e324df7e39
Vaibhav Jain <vaibhav.jain@betterplace.co.in>
2024-08-05 19:49:05 +0530

    Not needed

--- purgecss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  content: ["./src/components/**/*.js"], // Paths to your content files
-  css: [""], // Paths to your CSS files
-  output: "./purified.css", // Output file
-  safelist: [], // Add any CSS selectors you want to keep
-};
