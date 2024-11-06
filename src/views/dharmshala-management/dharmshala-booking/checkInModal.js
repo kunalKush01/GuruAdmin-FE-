151076dc24269b6e580fc855cd9814e1ec80ffa5
the-onelazyDev <amitcse21@gmail.com>
2024-11-06 10:54:54 +0530

    dharmshala payment on multiple days fix, edit page pending

--- src/views/dharmshala-management/dharmshala-booking/checkInModal.js
+++ src/views/dharmshala-management/dharmshala-booking/checkInModal.js
@@ -124,11 +124,11 @@ const CheckInModal = ({ visible, onClose, booking, mode }) => {
         <RoomsContainer
           className="rooms-container"
           roomsData={booking?.rooms || []}
-          roomTypes={booking?.rooms.map(room => ({
+          roomTypes={booking?.rooms?.map(room => ({
             _id: room.roomTypeId, 
             name: room.roomTypeName 
           })) || []}
-          buildings={booking?.rooms.map(room => ({
+          buildings={booking?.rooms?.map(room => ({
             _id: room.building, 
             name: room.buildingName 
           })) || []}
