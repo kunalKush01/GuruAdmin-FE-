37d69fb04cbedfba7362e6deb9b026bb0b50619f
the-onelazyDev <amitcse21@gmail.com>
2025-01-15 11:13:58 +0530

    craco error fixed

--- craco.config.js
+++ craco.config.js
@@ -25,8 +25,14 @@ module.exports = {
       "@configs": path.resolve(__dirname, "src/configs"),
       "@utils": path.resolve(__dirname, "src/utility/Utils"),
       "@hooks": path.resolve(__dirname, "src/utility/hooks"),
+      'react/jsx-runtime': path.resolve(__dirname, 'node_modules/react/jsx-runtime'),
+      'react/jsx-dev-runtime': path.resolve(__dirname, 'node_modules/react/jsx-dev-runtime'),
     },
     configure: (webpackConfig) => {
+      if (!webpackConfig.resolve) webpackConfig.resolve = {};
+      if (!webpackConfig.resolve.fallback) webpackConfig.resolve.fallback = {};
+      webpackConfig.resolve.fallback['react/jsx-runtime'] = require.resolve('react/jsx-runtime');
+
       if (process.env.NODE_ENV === "production") {
         const terserPlugin = webpackConfig.optimization.minimizer.find(
           (plugin) =>
