a2375406e8f10a6d18559b7b0b4072e38f7e6598
kunal kush <kunalkuswah7480@gmail.com>
2025-04-01 17:55:48 +0530

    fix image upload downlaod function also fix news events notice

--- src/api/sharedStorageApi.js
+++ src/api/sharedStorageApi.js
@@ -1,42 +1,62 @@
-import axios from 'axios';
+import axios from "axios";
+import { selectAccessToken } from "../redux/authSlice";
+import { store } from "../redux/store";
 
-const REACT_APP_BASEURL_PUBLIC=process.env.REACT_APP_BASEURL_PUBLIC;
+const REACT_APP_BASEURL_PUBLIC = process.env.REACT_APP_BASEURL_PUBLIC;
+const REACT_APP_BASEURL = process.env.REACT_APP_BASEURL;
 
-export const uploadFile = async (payload,isMultiple) => {
+const trustId = localStorage.getItem("trustId");
+const accessToken = selectAccessToken(store.getState());
+const headers = {
+  Authorization: `Bearer ${accessToken}`,
+  "Content-Type": "multipart/form-data", // Important for file uploads
+};
+export const uploadFile = async (payload, isMultiple) => {
   try {
     //  const endpoint = isMultiple ? 'storage/upload/multiple' : 'storage/upload'
-     const endpoint = 'storage/upload'
-    const response = await axios.post(`${REACT_APP_BASEURL_PUBLIC}${endpoint}`, payload);
+    const endpoint = "storage/upload";
+    const response = await axios.post(
+      `${REACT_APP_BASEURL}${trustId}/${endpoint}`,
+      payload,
+      { headers }
+    );
     if (response.status === 200) {
       return response.data;
     } else {
-      throw new Error('Upload failed');
+      throw new Error("Upload failed");
     }
   } catch (error) {
-    console.error('Error uploading file:', error);
+    console.error("Error uploading file:", error);
     throw error;
   }
 };
 
 export const deleteFile = async (filepath, payload) => {
   try {
-    const response = await axios.delete(`${REACT_APP_BASEURL_PUBLIC}storage/delete/${filepath}`, payload);
+    const response = await axios.delete(
+      `${REACT_APP_BASEURL}storage/delete/${filepath}`,
+      payload
+    );
     return response.data;
   } catch (error) {
-    console.error('Error deleting file:', error);
+    console.error("Error deleting file:", error);
     throw error;
   }
 };
 
 export const downloadFile = async (filepath, params) => {
   try {
-    const response = await axios.get(`${REACT_APP_BASEURL_PUBLIC}storage/download/${filepath}`, {
-      responseType: 'blob',  
-      params, 
-    });
-    return response.data; 
+    const response = await axios.get(
+      `${REACT_APP_BASEURL_PUBLIC}storage/download/${filepath}`,
+      {
+        responseType: "blob",
+        params,
+        headers
+      }
+    );
+    return response.data;
   } catch (error) {
-    console.error('Error downloading file:', error);
+    console.error("Error downloading file:", error);
     throw error;
   }
 };
